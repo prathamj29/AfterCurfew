8be2eaedd17a19de2aba2dd6ab0255a714069e9d
Pratham <iamprathamj@gmail.com>
2026-03-03 02:22:48 +0530

    Update store via Admin Panel

--- js/app.js
+++ js/app.js
@@ -197,7 +197,7 @@ document.addEventListener('DOMContentLoaded', function () {
     "category": "snacks",
     "price": 89,
     "image": "images/1772126865445-484.png",
-    "inStock": false
+    "inStock": true
   },
   {
     "id": "c00b0988-8aac-4b56-a12e-4e7babc41c37",
